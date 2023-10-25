a34b3e3308a136afce29ce30f55b3fae92df4eeb
sh940701 <visioner2168@gmail.com>
2023-10-25 15:13:19 +0900

    module 단위 import 에서, provider 와 controller 를 개별 import 해주는 로직으로 변경 추후 module 로 분류할 예정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common'
-import { UsersModule } from './users/users.module'
-import { UsersController } from './users/users.controller';
+import { UsersController } from './users/users.controller'
+import { UsersService } from './users/users.service'
+import { EmailService } from './email/email.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [],
   controllers: [UsersController],
+  providers: [UsersService, EmailService],
 })
 export class AppModule {
 }
