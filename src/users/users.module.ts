2eecd1740e700e2bb986a805235a299603726e4a
sh940701 <visioner2168@gmail.com>
2023-10-27 14:54:14 +0900

    update: import Email module, remove unnecessary EmailService provider

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
-import { EmailService } from '../email/email.service'
+import { EmailModule } from '../email/email.module'
 
 @Module({
-  imports: [],
+  imports: [EmailModule],
   controllers: [UsersController],
-  providers: [UsersService, EmailService],
+  providers: [UsersService],
 })
 export class UsersModule {
 }
