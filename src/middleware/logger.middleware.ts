255d6ce82182a76b78b88deb53b9289ab098bbf9
sh940701 <visioner2168@gmail.com>
2023-10-30 22:19:46 +0900

    feat: add logger file

---
+++ src/middleware/logger.middleware.ts
@@ -0,0 +1,15 @@
+import { Injectable, NestMiddleware } from '@nestjs/common'
+import { NextFunction } from 'express'
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log('Request...')
+    next()
+  }
+}
+
+export function loggerFunc(req: Request, res: Response, next: NextFunction) {
+  console.log('request...')
+  next()
+}
