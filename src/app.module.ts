f513cd26bd6872d1d082ee6edc1074d6c4dfae84
sh940701 <visioner2168@gmail.com>
2023-10-30 12:01:50 +0900

    update: get *.env files from dist/

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common'
 import { UsersModule } from './users/users.module'
-import { ConfigModule, ConfigService } from '@nestjs/config'
+import { ConfigModule } from '@nestjs/config'
+import emailConfig from './config/emailConfig'
+import { validationSchema } from './config/validationSchema'
 
 @Module({
-  imports: [UsersModule, ConfigModule.forRoot({
-    envFilePath: (process.env.NODE_ENV === 'production') ? '.production.env'
-      : (process.env.NODE_ENV === 'stage') ? '.stage.env' : '.development.env',
-  })],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
+  ],
   controllers: [],
-  providers: [ConfigService],
+  providers: [],
 })
 export class AppModule {
 }
