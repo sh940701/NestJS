e867b46584957c64396464e7c953a0b5b5299042
sh940701 <visioner2168@gmail.com>
2023-10-30 15:56:31 +0900

    update: apply pipeline

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common'
+import { Body, Controller, DefaultValuePipe, Get, Param, ParseIntPipe, Post, Query } from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
 import { VerifyEmailDto } from './dto/verify-email.dto'
 import { UserLoginDto } from './dto/user-login.dto'
@@ -31,8 +31,18 @@ export class UsersController {
   }
 
   @Get('/:id')
-  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
+  async getUserInfo(@Param('id', ParseIntPipe) userId: string): Promise<UserInfo> {
     return await this.usersService.getUserInfo(userId)
+  }
+
+  @Get()
+  findAll(
+    @Query('offset', new DefaultValuePipe(0), ParseIntPipe) offset: number,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+  ) {
+    console.log(offset, limit)
+
+    // return this.usersService.findAll()
   }
 
 }
