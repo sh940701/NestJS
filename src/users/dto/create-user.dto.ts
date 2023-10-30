846e9b0f48ac9a2c7c940e2fc9eea151b945f72f
sh940701 <visioner2168@gmail.com>
2023-10-30 15:55:36 +0900

    update: create user dto use class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,25 @@
+import { IsEmail, IsString, Matches, MaxLength, MinLength } from 'class-validator'
+import { Transform } from 'class-transformer'
+import { BadRequestException } from '@nestjs/common'
+
 export class CreateUserDto {
+  @Transform(({ value, obj }) => {
+    if (obj.password.includes(obj.name.trim())) {
+      throw new BadRequestException('password 는 name 과 같은 문자열을 포함할 수 없습니다.')
+    }
+    return value.trim()
+  })
+  @IsString()
+  @MinLength(2)
+  @MaxLength(30)
   readonly name: string
+
+  @IsString()
+  @IsEmail()
+  @MaxLength(60)
   readonly email: string
+
+  @IsString()
+  @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
   readonly password: string
 }
