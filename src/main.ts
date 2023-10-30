aa03192cbb7b24f1e85981b2d8edad7b5904136f
sh940701 <visioner2168@gmail.com>
2023-10-30 22:20:10 +0900

    update: apply logger to global

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import { loggerFunc } from './middleware/logger.middleware'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }))
+  app.use(loggerFunc)
   await app.listen(3000)
 }
 
