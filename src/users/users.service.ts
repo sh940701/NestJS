20464fe49d2dcfdd6ae95c28cf48510ffdf284fd
sh940701 <visioner2168@gmail.com>
2023-10-31 15:14:30 +0900

    feat: add verify Email feature

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -92,7 +92,19 @@ export class UsersService {
   }
 
   async verifyEmail(signupVerifyToken: string): Promise<string> {
-    throw new Error('Method not implemented')
+    const user = await this.usersRepository.findOne({
+      where: { signupVerifyToken },
+    })
+
+    if (!user) {
+      throw new NotFoundException('유저가 존재하지 않습니다.')
+    }
+
+    return this.authService.login({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    })
   }
 
   async login(email: string, password: string): Promise<string> {
